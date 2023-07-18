3f1197a873e66522dc9ea04712e0be81e55967d0
tzc1234 <peopli9997@gmail.com>
2023-07-18 14:18:43 +0100

    Remove unused code

--- PhotoSearchAppTests/PhotoSearchUIIntegrationTests.swift
+++ PhotoSearchAppTests/PhotoSearchUIIntegrationTests.swift
@@ -1,5 +1,5 @@
 //
-//  PhotoSearchAppTests.swift
+//  PhotoSearchUIIntegrationTests.swift
 //  PhotoSearchAppTests
 //
 //  Created by Tsz-Lung on 18/07/2023.
@@ -8,7 +8,7 @@
 import XCTest
 @testable import PhotoSearchApp
 
-final class PhotoSearchAppTests: XCTestCase {
+final class PhotoSearchUIIntegrationTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
