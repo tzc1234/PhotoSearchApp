ad9ceef3b1e99958fb77d6fe9d40b4ee627cbdfc
tzc1234 <peopli9997@gmail.com>
2023-07-20 11:11:35 +0100

    Move LoadPhotosPublisherAdapter to its own file

--- PhotoSearchApp/Composition/PhotoSearchComposer.swift
+++ PhotoSearchApp/Composition/PhotoSearchComposer.swift
@@ -28,30 +28,6 @@ enum PhotoSearchComposer {
     }
 }
 
-final class LoadPhotosPublisherAdapter {
-    private var loadPhotosCancellable: Cancellable?
-    private let loadPhotosPublisher: (String) -> LoadPhotosPublisher
-    var presenter: PhotosPresenter?
-    
-    init(loadPhotosPublisher: @escaping (String) -> LoadPhotosPublisher) {
-        self.loadPhotosPublisher = loadPhotosPublisher
-    }
-    
-    func loadPhotos(by searchTerm: String) {
-        presenter?.didStartLoading()
-        
-        loadPhotosCancellable = loadPhotosPublisher(searchTerm)
-            .receive(on: DispatchQueue.immediateWhenOnMainQueueScheluder)
-            .sink(receiveCompletion: { [weak self] completion in
-                if case let .failure(error) = completion {
-                    self?.presenter?.didFinishLoading(with: error)
-                }
-            }, receiveValue: { [weak self] photos in
-                self?.presenter?.didFinishLoading(with: photos)
-            })
-    }
-}
-
 final class PhotosViewAdapter: PhotosView {
     private weak var viewController: PhotoSearchViewController?
     private let loadImagePublisher: (Photo) -> LoadImagePublisher
