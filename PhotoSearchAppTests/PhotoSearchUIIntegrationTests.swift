bd47425fe414b07c9934ef7bc3f1ddf77a61c5e4
tzc1234 <peopli9997@gmail.com>
2023-07-21 14:51:44 +0100

    Move LoaderSpy to its own file

--- PhotoSearchAppTests/PhotoSearchUIIntegrationTests.swift
+++ PhotoSearchAppTests/PhotoSearchUIIntegrationTests.swift
@@ -5,7 +5,6 @@
 //  Created by Tsz-Lung on 18/07/2023.
 //
 
-import Combine
 import XCTest
 @testable import PhotoSearchApp
 
@@ -468,69 +467,5 @@ final class PhotoSearchUIIntegrationTests: XCTestCase {
 
     private func anyNSError() -> NSError {
         NSError(domain: "any error", code: 0)
-    }
-    
-    private class LoaderSpy {
-        typealias LoadPhotosPublisher = PassthroughSubject<[Photo], Error>
-        
-        private var loadPhotosRequests = [(publisher: LoadPhotosPublisher, searchTerm: String)]()
-        var loadPhotosCallCount: Int {
-            loadPhotosRequests.count
-        }
-        var loggedSearchTerms: [String] {
-            loadPhotosRequests.map(\.searchTerm)
-        }
-        
-        private(set) var cancelLoadCallCount = 0
-        
-        func loadPhotosPublisher(searchTerm: String) -> AnyPublisher<[Photo], Error> {
-            let publisher = LoadPhotosPublisher()
-            loadPhotosRequests.append((publisher, searchTerm))
-            return publisher.handleEvents(receiveCancel: { [weak self] in
-                self?.cancelLoadCallCount += 1
-            }).eraseToAnyPublisher()
-        }
-        
-        func completePhotosLoad(with photos: [Photo], at index: Int) {
-            guard index < loadPhotosRequests.count else { return }
-            
-            loadPhotosRequests[index].publisher.send(photos)
-            loadPhotosRequests[index].publisher.send(completion: .finished)
-        }
-        
-        func completePhotosLoad(with error: Error, at index: Int) {
-            guard index < loadPhotosRequests.count else { return }
-            loadPhotosRequests[index].publisher.send(completion: .failure(error))
-        }
-        
-        // MARK: - Image data loader
-        typealias LoadImagePublisher = PassthroughSubject<Data, Error>
-        
-        private var loadImageRequests = [(publisher: LoadImagePublisher, photo: Photo)]()
-        var loggedPhotosForLoadImage: [Photo] {
-            loadImageRequests.map(\.photo)
-        }
-        
-        private(set) var cancelLoadImageCallCount = 0
-        
-        func loadImagePublisher(photo: Photo) -> AnyPublisher<Data, Error> {
-            let publisher = LoadImagePublisher()
-            loadImageRequests.append((publisher, photo))
-            return publisher.handleEvents(receiveCancel: { [weak self] in
-                self?.cancelLoadImageCallCount += 1
-            }).eraseToAnyPublisher()
-        }
-        
-        func completeImageLoad(with data: Data, at index: Int) {
-            guard index < loadImageRequests.count else { return }
-            
-            loadImageRequests[index].publisher.send(data)
-            loadImageRequests[index].publisher.send(completion: .finished)
-        }
-        
-        func completeImageLoad(with error: Error, at index: Int) {
-            guard index < loadImageRequests.count else { return }
-            loadImageRequests[index].publisher.send(completion: .failure(error))
-        }
     }
 }
