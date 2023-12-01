a9bfd429a34d2a2295a5e6e6e2bd056dfbe90c0e
tzc1234 <peopli9997@gmail.com>
2023-12-01 14:33:48 +0000

    ImageDataCache does not message store upon creation in load image data use case

---
+++ PhotoSearchAppTests/Cache/LoadImageDataFromCacheUseCaseTests.swift
@@ -0,0 +1,27 @@
+//
+//  LoadImageDataFromCacheUseCaseTests.swift
+//  PhotoSearchAppTests
+//
+//  Created by Tsz-Lung on 01/12/2023.
+//
+
+import XCTest
+@testable import PhotoSearchApp
+
+final class LoadImageDataFromCacheUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertTrue(store.messages.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ImageDataCacher, store: ImageDataStoreSpy) {
+        let store = ImageDataStoreSpy()
+        let sut = ImageDataCacher(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
