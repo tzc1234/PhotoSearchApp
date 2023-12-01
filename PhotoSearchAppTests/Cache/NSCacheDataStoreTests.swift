14e64b69b4620318fb9f0af4f0eeff6a30fc0ad0
tzc1234 <peopli9997@gmail.com>
2023-12-01 16:55:30 +0000

    NSCacheDataStore.retrieveData delivers cached data

--- PhotoSearchAppTests/Cache/NSCacheDataStoreTests.swift
+++ PhotoSearchAppTests/Cache/NSCacheDataStoreTests.swift
@@ -9,12 +9,20 @@ import XCTest
 @testable import PhotoSearchApp
 
 final class NSCacheDataStore {
+    private let cache: NSCache<NSString, NSData>
+    
     init() {
-        
+        self.cache = NSCache<NSString, NSData>()
+    }
+    
+    func insert(_ data: Data, for key: String, completion: @escaping (ImageDataStore.InsertResult) -> Void) {
+        cache.setObject(data as NSData, forKey: key as NSString)
+        completion(.success(()))
     }
     
     func retrieveData(for key: String, completion: @escaping (ImageDataStore.RetrieveResult) -> Void) {
-        completion(.success(nil))
+        let data = cache.object(forKey: key as NSString) as? Data
+        completion(.success(data))
     }
 }
 
@@ -22,14 +30,25 @@ final class NSCacheDataStoreTests: XCTestCase {
     func test_retrieveData_deliversNoDataWhenNoCachedData() {
         let sut = makeSUT()
         
-        expect(sut, toRetrieve: noData(), for: anyId())
+        expect(sut, toRetrieve: noData(), for: anyKey())
     }
     
     func test_retrieveDataTwice_deliversNoDataTwiceWhenNoCachedDataWithNoSideEffects() {
         let sut = makeSUT()
+        let key = anyKey()
         
-        expect(sut, toRetrieve: noData(), for: anyId())
-        expect(sut, toRetrieve: noData(), for: anyId())
+        expect(sut, toRetrieve: noData(), for: key)
+        expect(sut, toRetrieve: noData(), for: key)
+    }
+    
+    func test_retrieveData_deliversCachedData() {
+        let sut = makeSUT()
+        let cachedData = anyData()
+        let key = anyKey()
+        
+        insert(cachedData, for: key, into: sut)
+        
+        expect(sut, toRetrieve: .success(cachedData), for: key)
     }
     
     // MARK: - Helpers
@@ -38,6 +57,24 @@ final class NSCacheDataStoreTests: XCTestCase {
         let sut = NSCacheDataStore()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func insert(_ data: Data,
+                        for key: String,
+                        into sut: NSCacheDataStore,
+                        file: StaticString = #filePath,
+                        line: UInt = #line) {
+        let exp = expectation(description: "Wait for insertion completion")
+        sut.insert(data, for: key) { receivedResult in
+            switch receivedResult {
+            case .success:
+                break
+            case .failure:
+                XCTFail("Expect insertion successfully, got a failure instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
     
     private func expect(_ sut: NSCacheDataStore,
@@ -62,5 +99,9 @@ final class NSCacheDataStoreTests: XCTestCase {
     
     private func noData() -> ImageDataStore.RetrieveResult {
         .success(nil)
+    }
+    
+    private func anyKey() -> String {
+        "any key"
     }
 }
