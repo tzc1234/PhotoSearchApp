54f6f7317709fe6b5fd62bbc682da276cbd49829
tzc1234 <peopli9997@gmail.com>
2023-12-01 11:36:58 +0000

    Introduce makeSUT to reduce duplications

--- PhotoSearchAppTests/Cache/CacheImageDataUseCaseTests.swift
+++ PhotoSearchAppTests/Cache/CacheImageDataUseCaseTests.swift
@@ -8,9 +8,9 @@
 import XCTest
 
 final class ImageDataCacher {
-    private let store: StoreSpy
+    private let store: CacheImageDataUseCaseTests.StoreSpy
     
-    init(store: StoreSpy) {
+    init(store: CacheImageDataUseCaseTests.StoreSpy) {
         self.store = store
     }
     
@@ -19,34 +19,42 @@ final class ImageDataCacher {
     }
 }
 
-final class StoreSpy {
-    enum Message: Equatable {
-        case insert(Data, for: String)
-    }
-    
-    private(set) var messages = [Message]()
-    
-    func insert(data: Data, for key: String) {
-        messages.append(.insert(data, for: key))
-    }
-}
-
 final class CacheImageDataUseCaseTests: XCTestCase {
     func test_init_doesNotMessageStoreUponCreation() {
-        let store = StoreSpy()
-        _ = ImageDataCacher(store: store)
+        let (_, store) = makeSUT()
         
         XCTAssertTrue(store.messages.isEmpty)
     }
     
     func test_saveData_requestsStoreCachingWithDataAndId() {
-        let store = StoreSpy()
-        let sut = ImageDataCacher(store: store)
+        let (sut, store) = makeSUT()
         let data = Data("save data".utf8)
         let id = "image id"
         
         sut.save(data: data, for: id)
         
         XCTAssertEqual(store.messages, [.insert(data, for: id)])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ImageDataCacher, store: StoreSpy) {
+        let store = StoreSpy()
+        let sut = ImageDataCacher(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    final class StoreSpy {
+        enum Message: Equatable {
+            case insert(Data, for: String)
+        }
+        
+        private(set) var messages = [Message]()
+        
+        func insert(data: Data, for key: String) {
+            messages.append(.insert(data, for: key))
+        }
     }
 }
