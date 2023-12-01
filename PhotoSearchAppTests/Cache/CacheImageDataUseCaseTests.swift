ce2add68f8218305d630c857942f4f8fe9e8e872
tzc1234 <peopli9997@gmail.com>
2023-12-01 11:33:03 +0000

    ImageDataCacher request data caching with data and id

--- PhotoSearchAppTests/Cache/CacheImageDataUseCaseTests.swift
+++ PhotoSearchAppTests/Cache/CacheImageDataUseCaseTests.swift
@@ -8,13 +8,27 @@
 import XCTest
 
 final class ImageDataCacher {
+    private let store: StoreSpy
+    
     init(store: StoreSpy) {
-        
+        self.store = store
+    }
+    
+    func save(data: Data, for id: String) {
+        store.insert(data: data, for: id)
     }
 }
 
 final class StoreSpy {
-    private(set) var messages = [Any]()
+    enum Message: Equatable {
+        case insert(Data, for: String)
+    }
+    
+    private(set) var messages = [Message]()
+    
+    func insert(data: Data, for key: String) {
+        messages.append(.insert(data, for: key))
+    }
 }
 
 final class CacheImageDataUseCaseTests: XCTestCase {
@@ -23,5 +37,16 @@ final class CacheImageDataUseCaseTests: XCTestCase {
         _ = ImageDataCacher(store: store)
         
         XCTAssertTrue(store.messages.isEmpty)
+    }
+    
+    func test_saveData_requestsStoreCachingWithDataAndId() {
+        let store = StoreSpy()
+        let sut = ImageDataCacher(store: store)
+        let data = Data("save data".utf8)
+        let id = "image id"
+        
+        sut.save(data: data, for: id)
+        
+        XCTAssertEqual(store.messages, [.insert(data, for: id)])
     }
 }
