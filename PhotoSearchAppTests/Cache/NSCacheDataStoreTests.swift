c0df602e6e437c0aaa897195fcc62de31f3feb7d
tzc1234 <peopli9997@gmail.com>
2023-12-01 16:32:23 +0000

    NSCacheDataStore.retrieveData twice delivers no data twice when no cached data

--- PhotoSearchAppTests/Cache/NSCacheDataStoreTests.swift
+++ PhotoSearchAppTests/Cache/NSCacheDataStoreTests.swift
@@ -20,18 +20,47 @@ final class NSCacheDataStore {
 
 final class NSCacheDataStoreTests: XCTestCase {
     func test_retrieveData_deliversNoDataWhenNoCachedData() {
-        let sut = NSCacheDataStore()
+        let sut = makeSUT()
         
-        let exp = expectation(description: "Wait for completion")
-        sut.retrieveData(for: anyId()) { receivedResult in
-            switch receivedResult {
-            case let .success(receivedData):
-                XCTAssertNil(receivedData)
-            case .failure:
-                XCTFail("Should not fail")
+        expect(sut, toRetrieve: noData(), for: anyId())
+    }
+    
+    func test_retrieveDataTwice_deliversNoDataTwiceWhenNoCachedDataWithNoSideEffects() {
+        let sut = makeSUT()
+        
+        expect(sut, toRetrieve: noData(), for: anyId())
+        expect(sut, toRetrieve: noData(), for: anyId())
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> NSCacheDataStore {
+        let sut = NSCacheDataStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func expect(_ sut: NSCacheDataStore,
+                        toRetrieve expectedResult: ImageDataStore.RetrieveResult,
+                        for key: String,
+                        file: StaticString = #filePath,
+                        line: UInt = #line) {
+        let exp = expectation(description: "Wait for retrieval completion")
+        sut.retrieveData(for: key) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+            case (.failure, .failure):
+                break
+            default:
+                XCTFail("Expect \(expectedResult), got \(receivedResult) instead", file: file, line: line)
             }
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
+    }
+    
+    private func noData() -> ImageDataStore.RetrieveResult {
+        .success(nil)
     }
 }
