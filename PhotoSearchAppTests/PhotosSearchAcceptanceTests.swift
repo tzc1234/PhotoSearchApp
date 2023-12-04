243d2b0964f2ba021ab3de02d9f3e3c199c2f1de
tzc1234 <peopli9997@gmail.com>
2023-12-04 12:44:52 +0000

    Move HTTPClientStub to its own file

--- PhotoSearchAppTests/PhotosSearchAcceptanceTests.swift
+++ PhotoSearchAppTests/PhotosSearchAcceptanceTests.swift
@@ -136,29 +136,4 @@ final class PhotosSearchAcceptanceTests: XCTestCase {
     private func searchKeyword() -> String {
         "keyword"
     }
-    
-    final class HTTPClientStub: HTTPClient {
-        private let stub: (URL) -> HTTPClient.Result
-        
-        init(stub: @escaping (URL) -> HTTPClient.Result) {
-            self.stub = stub
-        }
-        
-        private struct Task: HTTPClientTask {
-            func cancel() {}
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            completion(stub(url))
-            return Task()
-        }
-        
-        static var offline: Self {
-            .init(stub: { _ in .failure(anyNSError()) })
-        }
-        
-        static func online(_ response: @escaping (URL) -> (Data, HTTPURLResponse)) -> Self {
-            .init(stub: { .success(response($0)) })
-        }
-    }
 }
